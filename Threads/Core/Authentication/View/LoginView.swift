87f5969cfe9fb3955192f27cee5846b0dd89e36b
Edgar Vergara <edgarvm13@hotmail.com>
2023-09-02 14:56:47 -0600

    initial project setup

---
+++ Threads/Core/Authentication/View/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  Threads
+//
+//  Created by Edgar Vergara on 02/09/23.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
