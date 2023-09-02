90a87815d5ee9637e3d663ee2f57d150d6097da6
Edgar Vergara <edgarvm13@hotmail.com>
2023-09-02 14:51:19 -0600

    Initial Commit

---
+++ Threads/ThreadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsApp.swift
+//  Threads
+//
+//  Created by Edgar Vergara on 02/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
