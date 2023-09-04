e92e03af663d5bff4853bbbd94a4a92d84a7292b
Edgar Vergara <edgarvm13@hotmail.com>
2023-09-04 10:05:21 -0600

    feed view ui

--- Threads/Core/Feed/View/FeedView.swift
+++ Threads/Core/Feed/View/FeedView.swift
@@ -9,7 +9,30 @@ import SwiftUI
 
 struct FeedView: View {
     var body: some View {
-        Text("Feed")
+        NavigationStack {
+            ScrollView(showsIndicators: false) {
+                LazyVStack {
+                    ForEach(0 ... 10, id: \.self) { thread in
+                        ThreadCell()
+                    }
+                }
+            }
+            .refreshable {
+                print("DEBUG: refresh threads")
+            }
+            .navigationTitle("Threads")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "arrow.counterclockwise")
+                            .foregroundColor(.black)
+                    }
+                }
+            }
+        }
     }
 }
 
---
+++ Threads/Core/Feed/View/ThreadCell.swift
@@ -0,0 +1,88 @@
+//
+//  ThreadCell.swift
+//  Threads
+//
+//  Created by Edgar Vergara on 04/09/23.
+//
+
+import SwiftUI
+
+struct ThreadCell: View {
+    var body: some View {
+        VStack {
+            HStack(alignment: .top, spacing: 12) {
+                Image("threads-logo")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 40, height: 40)
+                    .clipShape(Circle())
+                
+                // username and thread caption
+                VStack(alignment: .leading, spacing: 4) {
+                    HStack {
+                        Text("maxverstappen1")
+                            .font(.footnote)
+                        .fontWeight(.semibold)
+                        
+                        Spacer()
+                        
+                        Text("10 m")
+                            .font(.caption)
+                            .foregroundColor(Color(.systemGray3))
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "ellipsis")
+                                .foregroundColor(Color(.darkGray))
+                        }
+
+                    }
+                        
+                    Text("formula 1 champion")
+                        .font(.footnote)
+                        .multilineTextAlignment(.leading)
+                    
+                    HStack(spacing: 16) {
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "heart")
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "bubble.right")
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "arrow.rectanglepath")
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "paperplane")
+                        }
+                    }
+                    .padding(.bottom, 8)
+                    .foregroundColor(.black)
+                }
+            }
+            .padding()
+            
+            Divider()
+                .padding(.horizontal)
+        }
+        
+    }
+}
+
+struct ThreadCell_Previews: PreviewProvider {
+    static var previews: some View {
+        ThreadCell()
+    }
+}
