1dba6076e547d5195d8f8d5059ef632dff7fae3b
Edgar Vergara <edgarvm13@hotmail.com>
2023-10-17 21:55:46 +0200

    create thread ui

--- Threads/Core/TabBar/ThreadsTabView.swift
+++ Threads/Core/TabBar/ThreadsTabView.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 struct ThreadsTabView: View {
     @State var selectedTab = 0
+    @State var previousTab = 0
+    
+    @State private var showCreateThreadView = false
+    
     var body: some View {
         TabView(selection: $selectedTab) {
             FeedView()
@@ -16,21 +20,21 @@ struct ThreadsTabView: View {
                     Image(systemName: selectedTab == 0 ? "house.fill" : "house")
                         .environment(\.symbolVariants, selectedTab == 0 ? .fill : .none)
                 }
-                .onAppear { selectedTab = 0 }
+                //.onAppear { selectedTab = 0 }
                 .tag(0)
             
             ExploreView()
                 .tabItem {
                     Image(systemName: "magnifyingglass")
                 }
-                .onAppear { selectedTab = 1 }
+                //.onAppear { selectedTab = 1 }
                 .tag(1)
             
-            ThreadCreationView()
+            Text("")
                 .tabItem {
                     Image(systemName: "plus")
                 }
-                .onAppear { selectedTab = 2 }
+                //.onAppear { selectedTab = 2 }
                 .tag(2)
             
             ActivityView()
@@ -38,7 +42,7 @@ struct ThreadsTabView: View {
                     Image(systemName: selectedTab == 3 ? "heart.fill" : "heart")
                         .environment(\.symbolVariants, selectedTab == 3 ? .fill : .none)
                 }
-                .onAppear { selectedTab = 3 }
+                //.onAppear { selectedTab = 3 }
                 .tag(3)
             
             ProfileView()
@@ -46,9 +50,23 @@ struct ThreadsTabView: View {
                     Image(systemName: selectedTab == 4 ? "person.fill" : "person")
                         .environment(\.symbolVariants, selectedTab == 4 ? .fill : .none)
                 }
-                .onAppear { selectedTab = 4 }
+                //.onAppear { selectedTab = 4 }
                 .tag(4)
         }
+        .onChange(of: selectedTab, perform: { newValue in
+            showCreateThreadView = selectedTab == 2
+
+            if !showCreateThreadView {
+                previousTab = selectedTab
+            }
+            
+            selectedTab = newValue
+        })
+        .sheet(
+            isPresented: $showCreateThreadView,
+            onDismiss: { selectedTab = previousTab },
+            content: { ThreadCreationView()}
+        )
         .tint(.black)
     }
 }
--- Threads/Core/ThreadCreation/View/ThreadCreationView.swift
+++ Threads/Core/ThreadCreation/View/ThreadCreationView.swift
@@ -8,8 +8,65 @@
 import SwiftUI
 
 struct ThreadCreationView: View {
+    
+    @State var caption = ""
+    
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        Text("ThreadCreationView")
+        NavigationStack {
+            VStack {
+                HStack(alignment: .top) {
+                    CircularProfileImageView()
+                    
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("maxverstappen1")
+                            .fontWeight(.semibold)
+                        
+                        TextField("Start thread...", text: $caption, axis: .vertical)
+                    }
+                    .font(.footnote)
+                    
+                    Spacer()
+                    
+                    if !caption.isEmpty {
+                        Button(action: {
+                            caption = ""
+                        }, label: {
+                            Image(systemName: "xmark")
+                                .resizable()
+                                .frame(width: 12, height: 12)
+                                .foregroundColor(.gray)
+                        })
+                    }
+                }
+                
+                Spacer()
+            }
+            .padding()
+            .navigationTitle("New Thread")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button("Cancel") {
+                        dismiss()
+                    }
+                    .font(.subheadline)
+                    .foregroundStyle(.black)
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Post") {
+                        
+                    }
+                    .opacity(caption.isEmpty ? 0.5 : 1.0)
+                    .disabled(caption.isEmpty)
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.black)
+                }
+            }
+        }
     }
 }
 
